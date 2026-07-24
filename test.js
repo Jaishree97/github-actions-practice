bff96624bfe64b303febcc1979874ae4029aba9c
Jaishree Chaure <jaishreechoure2016@gmail.com>
2026-07-24 15:48:41 +0000

    updated test.js again

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ console.log("Running tests...");
 
 if (2 + 2 === 4) {
   console.log(" Test passed");
-  process.exit(1);
+  process.exit(0);
 } else {
   console.log("Test failed");
   process.exit(1);
